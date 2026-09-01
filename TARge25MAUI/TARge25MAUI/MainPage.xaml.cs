81749c5c336fdd053c21438a1cea926bc93f174b
Kundratsk <kundratsk@gmail.com>
2026-09-01 15:14:33 +0300

    Update MainPage.xaml.cs

--- TARge25MAUI/TARge25MAUI/MainPage.xaml.cs
+++ TARge25MAUI/TARge25MAUI/MainPage.xaml.cs
@@ -14,6 +14,24 @@
             count++;
             CounterBtn.Text = $"Vajutatud {count} korda";
 
+            // Kui loendur on 5 või rohkem, muuda nupu värvi
+            if (count >= 5)
+            {
+                CounterBtn.BackgroundColor = Colors.Red;
+                CounterBtn.TextColor = Colors.White;
+            }
+
+            if (count <= 10)
+            {
+                BotImage.Scale += 0.1;
+            }
+
+            if (count >= 10)
+            {
+                BotImage.IsVisible = false; // Peidab pildi
+                CounterLabel.Text = "Pilt kadus ära! Vajuta Reset.";
+            }
+
             // Genereerime juhusliku värvi (R, G, B)
             var random = new Random();
             var randomColor = Color.FromRgb(
@@ -34,7 +52,25 @@
 
             CounterLabel.Text = "Alustame uuesti!";
 
+            BotImage.Scale = 1;
+
+            // VÕI teeme loogika: kui on vasakul, liiguta paremale, ja vastupidi
+            if (BotImage.HorizontalOptions == LayoutOptions.Start)
+            {
+                BotImage.HorizontalOptions = LayoutOptions.End;
+            }
+            else
+            {
+                BotImage.HorizontalOptions = LayoutOptions.Start;
+            }
+
             BotImage.Rotation = 0;
+
+            CounterBtn.ClearValue(BackgroundColorProperty);// eemaldab counter nupu taustavärvi
+            CounterBtn.ClearValue(Button.TextColorProperty);// eemaldab counter nupu tekstivärvi
+
+
+            BotImage.IsVisible = true; // Toob pildi tagasi
         }
     }
 }
