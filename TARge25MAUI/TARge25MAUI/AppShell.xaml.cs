63b5929c69bc1171d1053972eebbb260c2a78102
Kundratsk <kundratsk@gmail.com>
2026-09-01 14:25:55 +0300

    Commit

---
+++ TARge25MAUI/TARge25MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TARge25MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TARge25MAUI/TARge25MAUI/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace TARge25MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            count++;
+            CounterBtn.Text = $"Vajutatud {count} korda";
+
+            // Genereerime juhusliku värvi (R, G, B)
+            var random = new Random();
+            var randomColor = Color.FromRgb(
+                random.Next(0, 256), // Red
+                random.Next(0, 256), // Green
+                random.Next(0, 256)  // Blue
+            );
+            BotImage.Rotation += 15;
+            // Rakendame värvi teisele nupule või taustale
+            ResetBtn.BackgroundColor = randomColor;
+        }
+
+        private void OnResetClicked(object? sender, EventArgs e)
+        {
+            count = 0;
+
+            CounterBtn.Text = "Vajuta mind";
+
+            CounterLabel.Text = "Alustame uuesti!";
+
+            BotImage.Rotation = 0;
+        }
+    }
+}
