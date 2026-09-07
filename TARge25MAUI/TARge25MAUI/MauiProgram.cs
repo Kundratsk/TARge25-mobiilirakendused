d74d6f90addc5af57650d1d9ceed05d05ae164fb
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-07 12:45:38 +0300

    Update

--- TARge25MAUI/TARge25MAUI/MauiProgram.cs
+++ TARge25MAUI/TARge25MAUI/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace TARge25MAUI
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("SoCafe.otf", "Socafe");
                 });
 
 #if DEBUG
